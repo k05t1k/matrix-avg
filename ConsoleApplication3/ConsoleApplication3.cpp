0765b5e73cfea01921e7d10443704744996d31af
k05t1k <isip_k.m.fionov@mpt.ru>
2024-02-17 09:05:11 +0300

    first commit

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float _array[3][5] = { };
+	float some_number = 0;
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int k = 0; k < 5; k++)
+		{
+			cin >> _array[i][k];
+			some_number += _array[i][k];
+		}
+	}
+
+	cout << some_number / 15;
+}
